c3f9cdf4b60e0c12981a02134cc942bb04ea5f72
Abhishek <jadonabhi0@gmail.com>
2022-07-01 01:08:33 +0530

    update

--- variables/variable.js
+++ variables/variable.js
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+
+// 4 Ways to Declare a JavaScript Variable:
+//     Using var
+//     Using let
+//     Using const
+//     Using nothing
+
+
+
+// Variable names are case-sensitive in JavaScript. So, the variable names msg, MSG, Msg, 
+//mSg are considered separate variables.
+// Variable names can contain letters, digits, or the symbols $ and _.
+// A variable name cannot start with a digit 0-9.
+// A variable name cannot be a reserved keyword in JavaScript, e.g. var, function, return cannot be variable names.
+
+var name = "abhishek jadon";
+console.log(name);
+
+
+
