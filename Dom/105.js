740b3bba6787380e25d199ea3375fac0adf0703c
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-12-18 19:37:48 +0530

    Update 105.js

--- Dom/105.js
+++ Dom/105.js
@@ -3,6 +3,8 @@
 const mainHeading = document.querySelector("#main-heading");
 // console.log(mainHeading.style);
 
+
+
 mainHeading.style.color = "yellow";
 mainHeading.style.border = "2px solid yellow"
 mainHeading.style.padding = "5px"
