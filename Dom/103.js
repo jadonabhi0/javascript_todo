8067e9316fad41edee6747c4281aaafbbef3fe3a
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2022-12-19 23:32:53 +0530

    Update 103.js

--- Dom/103.js
+++ Dom/103.js
@@ -8,5 +8,7 @@ console.log(mainHeading);
 const header = document.querySelector(".header");
 console.log(header);
 
+
+
 const naveItem = document.querySelectorAll(".nav-item"); // return all the same class obj
 console.log(naveItem);
